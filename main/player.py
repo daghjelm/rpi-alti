5d2179d1e674d841d7c04d929a43f2b4e8e8a136
Dag <dag.hjelm@gmail.com>
2023-07-28 14:08:03 +0100

    cleanup while loop and direction logic

--- main/player.py
+++ main/player.py
@@ -1,16 +1,21 @@
 from time import sleep
 from yoctopuce.yocto_api import YAPI
+from yoctopuce.yocto_altitude import YAltitude
+from vlc import MediaPlayer
 
 class Player():
     def __init__(
         self, 
-        player,
-        sensor,
-        margin,
+        player: MediaPlayer,
+        sensor: YAltitude,
+        margin: float,
+        play_time: int,
+        interval: float
     ):
         self.player = player
         self.sensor = sensor
         self.margin = margin * 1000 #need margin in ms
+        self.play_time = play_time * 1000 #as ms
 
         self.full_time = player.get_length()
         self.half_time = self.full_time / 2
@@ -18,12 +23,12 @@ class Player():
     # def get_adjusted_duration(self, pos, duration):
     #     return duration + (1 - pos % 1)
 
-    def play_video(self, pos, up, duration):
+    def play_video_fixed(self, pos, up, duration):
         end_time = self.half_time if up else self.full_time
         #we shouldn't play longer than the video - the margin
         if pos < end_time - self.margin - duration:
             self.player.set_rate(1)
-            # self.player.play()
+            self.player.play()
             print("duration", duration)
             YAPI.Sleep(duration)
             self.player.set_rate(0.25)
@@ -31,48 +36,44 @@ class Player():
         #this is if sleeping the duration will put us past end - margin
         elif end_time - self.margin - pos > 0:
             self.player.set_rate(1)
-            # self.player.play()
+            self.player.play()
             print("end_time - self.margin - pos", end_time - self.margin - pos)
             YAPI.Sleep(end_time - self.margin - pos)
             self.player.set_rate(0.25)
+        
+    def calc_dir_and_play(self, prev, diff):
+        pos = self.player.get_time()
+        current = self.sensor.get_currentValue()
 
-    def run(self, play_time, interval):
-        sensor = self.sensor
-        player = self.player
-        play_time = play_time * 1000
-        pos = player.get_time()
+        going_up = current - prev > diff
+        going_down = current - prev < -diff
+
+        direction = "up" if going_up else "down" if going_down else "still"
+        print("direction", direction)
+
+        changed_direction = (going_up and pos > self.half_time) or \
+                            (going_down and pos < self.half_time)
+
+        if going_up or going_down:
+            if changed_direction:
+                pos = self.full_time - pos
+                self.player.set_time(pos)
+            try:
+                self.play_video_fixed(pos, going_up, self.play_time)
+            except Exception as e:
+                print(e)
+        #sensor is still
+        else:
+            print('still')
+
+
+    def run(self):
         diff = 0.15
 
         #get first value
-        prev = sensor.get_currentValue()
+        prev = self.sensor.get_currentValue()
 
-        while sensor.isOnline():
-            pos = player.get_time()
-            current = sensor.get_currentValue()
-            #sensor is moving up
-            if current - prev > diff:
-                print('up')
-                if pos > self.half_time: #changed direction
-                    pos = self.full_time - pos
-                    player.set_time(pos)
-                try:
-                    self.play_video(pos, True, play_time)
-                except Exception as e:
-                    print(e)
-            #sensor if moving down
-            elif current - prev < -diff:
-                print('down')
-                if pos < self.half_time: #means we changed direction
-                    #calculate new position
-                    pos = self.full_time - pos
-                    player.set_time(pos)
-                try:
-                    self.play_video(pos, False, play_time)
-                except Exception as e:
-                    print(e)
-            #sensor is still
-            else:
-                print('still')
-
-            prev = sensor.get_currentValue()
-            sleep(interval)
+        while self.sensor.isOnline():
+            self.calc_dir_and_play(prev, diff)
+            prev = self.sensor.get_currentValue()
+            sleep(self.interval)
