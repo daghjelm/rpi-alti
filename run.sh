32284af7e2a139e008d4ec026f7d03f79845e7e2
Dag <dag.hjelm@gmail.com>
2024-01-06 13:28:31 +0100

    remove debug and keyboard flags

--- run.sh
+++ run.sh
@@ -9,4 +9,5 @@ RPI_MOVIES_DIR="${DOCUMENTS_DIR}/rpi-movies"
 export DISPLAY=:0
 
 # Run the Python script with arguments
-python3 "${RPI_ALTI_DIR}/main.py" -v="${RPI_MOVIES_DIR}/5NY_EKAR.mp4" -a="${RPI_SOUNDS_DIR}/EK.wav" -s=5 -t -p=10 -b=600 --diff=0.7 -i=1 -k -d
+#python3 "${RPI_ALTI_DIR}/main.py" -v="${RPI_MOVIES_DIR}/5NY_EKAR.mp4" -a="${RPI_SOUNDS_DIR}/EK.wav" -s=20 -t -p=5 -b=600 --diff=0.7 -i=1 -k -d
+python3 "${RPI_ALTI_DIR}/main.py" -v="${RPI_MOVIES_DIR}/5NY_EKAR.mp4" -a="${RPI_SOUNDS_DIR}/EK.wav" -s=20 -t -p=5 -b=600 --diff=0.7 -i=1
